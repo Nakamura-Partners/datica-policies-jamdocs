149ae91be0d36f33c84c3250c7fc6bb67de98d1c
padraigmacgabann <39102147+padraigmacgabann@users.noreply.github.com>
2021-01-22 03:02:42 -0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,6 +15,7 @@
   </Layout>
 </template>
 
+<!--
 <script>
 import GitLink from '~/components/GitLink.vue'
 import Shortcut from '~/components/Shortcut.vue'
@@ -32,6 +33,7 @@ export default {
   }
 }
 </script>
+-->
 
 <style lang="scss" scoped>
 .content {
